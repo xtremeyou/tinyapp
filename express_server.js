ddd50d72915c81a9aec900a2e125b29b82aaedeb
Merak Bourque <xtremeyou@hotmail.com>
2024-07-26 18:53:00 -0400

    add new endpoint '/hello'

--- express_server.js
+++ express_server.js
@@ -8,7 +8,11 @@ const urlDatatbase = {
 };
 
 app.get('/urls.json', (req, res) => {
-  res.json(urlDatatbase)
+  res.json(urlDatatbase);
+});
+
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Herro <b>World</b></body></html>\n');
 });
 
 app.get('/', (req, res) => {
