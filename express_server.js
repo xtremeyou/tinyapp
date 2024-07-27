9e1adf99c09cde6c31db60caa4e6367077094880
Merak Bourque <xtremeyou@hotmail.com>
2024-07-27 11:05:32 -0400

    add comments to express_server.js

--- express_server.js
+++ express_server.js
@@ -4,20 +4,24 @@ const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
+//creates a database to use our templates
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
+//sends a json respones containing the data of the urldatabase object
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+//lets us use our template vars in our views
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
+//when using the small data links inside urldatabse it'll output the larger one on screen
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
@@ -25,10 +29,12 @@ app.get("/urls/:id", (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+//just sends a response/message to the path /url, which says herro world
 app.get('/hello', (req, res) => {
   res.send('<html><body>Herro <b>World</b></body></html>\n');
 });
 
+//says hello when at endpoint
 app.get('/', (req, res) => {
   res.send('Hello');
 });
