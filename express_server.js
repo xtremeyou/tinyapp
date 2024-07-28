7a3801190401a093a741895f0f4ad734a64954c9
Merak Bourque <xtremeyou@hotmail.com>
2024-07-28 15:33:35 -0400

    add comments to express_server.js, add new post method for updating long urls, and add html to corresponding method

--- express_server.js
+++ express_server.js
@@ -15,7 +15,7 @@ const generateRandomString = () => {
 
   return randomString;
 };
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); //allow us to read req.params.body in human readable form
 
 //creates a database to use our templates
 const urlDatabase = {
@@ -23,60 +23,66 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com'
 };
 
-//sends a json respones containing the data of the urldatabase object
 app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
+  res.json(urlDatabase); //sends urlDatabase to path /urls.json in json form
 });
 
 //lets us use our template vars in our views
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
+  const templateVars = { urls: urlDatabase }; //allows us to use URLdatabase key/value pairs inside our urls_index view file
+  res.render('urls_index', templateVars); //renders the page, and allows use of templateVars
 });
 
 //posts data from longURl to the database with a generated shortURl
 //console.logs both longURl and shortURl
 //redirects shortURl to urls_show view
 app.post('/urls', (req, res) => {
-  const longURL = req.body.longURL;
-  const shortURLID = generateRandomString();
-  urlDatabase[shortURLID] = longURL;
-  console.log(`longURL: ${longURL}`);
-  console.log(`short URL ID: ${shortURLID}`);
-  res.redirect('/urls');
+  const longURL = req.body.longURL; //assigns variable longURl to req.body.longURl, which allows variable longURl to have longURl's body data
+  const shortURLID = generateRandomString(); //assigns variable shortURLID a random string of 6 characters
+  urlDatabase[shortURLID] = longURL; // shortURlID now replaces urlDatabases key, and a longURl to it as a value?
+  res.redirect('/urls'); //redirects to the urls list page, where it contains a list of all urls in the database
+});
+
+app.post('/urls/:id', (req, res) => {
+  const id = req.params.id; //assigns id to id of the route path
+  const newLongURL = req.body.longURL; //assign newLongURL the data of the longURL data from the form submitted
+  urlDatabase[id] = newLongURL; //assigns key of urlDatabase a newlongURL value
+  res.redirect('/urls'); //redirects to path /urls which is the page that lists our created urls
 });
 
 //connects the view file to our server as well as adding a path to it
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  res.render('urls_new'); //renders a new view "urls_new"
+});
+
+
+app.get('/u/:id', (req, res) => {
+  const id = req.params.id; //assigns id to websites path /urls/id
+  const longURL = urlDatabase[id]; //assigns longurl variable to urlDatabases id, which is the value of URLDatabases key/value pairs
+  res.redirect(longURL); //if urlDatabase has a longURL value attached to the key which is id, it'll redirect webpage to a new site
+});
+
+//outputs long url id, and shortURl id on path /urls/:id
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id; //assigns id to webites path /urls/:id
+  const longURL = urlDatabase[id]; //assigns longURl to urlDatabases key, it'll now hold the long version of an url
+  const templateVars = { id: req.params.id, longURL: longURL }; //allows us to use the id and longURL variables inside our views using js
+  res.render('urls_show', templateVars); //it'll now render the views template, allowing us to access it, as well as given us access to the objects key/value pairs data
 });
 
 app.post('/urls/:id/delete', (req, res) => {
-  const id = req.params.id;
-  delete urlDatabase[id];
-  res.redirect('/urls');
-});
-//redirects shortURL to longUrl in database, so for example b2xVn2 redirects to lighthouselab.com
-app.get('/u/:id', (req, res) => {
-  const shortURLID = req.params.id;
-  const longURL = urlDatabase[shortURLID];
-  res.redirect(longURL);
-});
-
-//when using the small data links inside urldatabse it'll output the larger one on screen
-app.get("/urls/:id", (req, res) => {
-  const id = req.params.id;
-  const longURL = urlDatabase[id];
-  const templateVars = { id: req.params.id, longURL: longURL };
-  res.render('urls_show', templateVars);
+  const id = req.params.id; //assigns id to website path /urls/:id/delete
+  delete urlDatabase[id]; //using the delete method, it'll delete the key/value pairs inside urlDatabase
+  res.redirect('/urls'); //it'll redirect us to path /urls after we delete everything
 });
 
 
 //says hello when at endpoint
 app.get('/', (req, res) => {
-  res.send('Hello');
+  res.send('Hello'); //sends "Hello" to / endpoint path
 });
 
+//allows server to listen for events
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
