86278eb9b1c0b05c03bd1434fdece731460cd928
Merak Bourque <xtremeyou@hotmail.com>
2024-07-28 20:37:15 -0400

    add cookie parser code to express_server

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080;
 
+app.use(cookieParser());
 
 app.use(express.urlencoded({ extended: true })); //allow us to read req.params.body in human readable form
 
